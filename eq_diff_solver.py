ab7e3eedfd8a7a773c2b269a0d3a8e450be1753c
FraViss <francyvis96@gmail.com>
2023-04-16 17:26:27 +0200

    16-04-2023 commit
    Prove varie di correzione degli errori e tentativo di esecuzione codice.

--- eq_diff_solver.py
+++ eq_diff_solver.py
@@ -2,19 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.integrate import odeint
-from scipy.integrate import solve_ivp
 
-def odefun(t,x,params_log):
+
+def odefun(t,*x,**params_log):
 
     # Variables
-    Cs_ctnt = x[0]
-    Cc_ctnt = x[1]
-    Cp_ctnt = x[2]
+    Cs_ctnt = x[1]
+    Cc_ctnt = x[2]
+    Cp_ctnt = x[3]
 
     # Arguments
-    a_log = params_log[0]
-    b_log = params_log[1]
-    Tsc_log = params_log[2]
+    a_log = list(params_log.values())[0]
+    b_log = list(params_log.values())[1]
+    Tsc_log = list(params_log.values())[2]
 
     # cTnT
     Jsc_ctnt = Cs_ctnt - Cc_ctnt
@@ -23,6 +23,7 @@ def odefun(t,x,params_log):
 
     # sigmoid curve
     G_sc = np.power(t, 3) / (np.power(t, 3) + np.power(10, (3 * (Tsc_log))))
+
     #Differential equations
     dCs_ctnt_tau = - Jsc_ctnt * G_sc
     dCc_ctnt_tau = Jsc_ctnt * G_sc - Jcp_ctnt
@@ -34,14 +35,14 @@ def odefun(t,x,params_log):
     return d_concentration
 
 #initial conditions
-x0=[1,1,0]
 t=np.linspace(0,10,5)
-v=[1,1,0]
+x0=np.array([1,2,3])
+params_log=np.array([1,1,0])
 
 #solve
-sol=odeint(odefun,x0,t,args=(v,))
+sol=odeint(odefun, y0=x0, t=t, args=(params_log[0],params_log[1],params_log[2]))
 
 print(sol)
 
 #x=7,3,0
-#v=-0.23,-1.02,1.85
+#params_log=-0.23,-1.02,1.85
