700ea70a3c07a9c3d44cdb1b306a8b12d6d0d84e
FraViss <francyvis96@gmail.com>
2023-10-02 11:39:33 +0200

    02-10-2023 commit
    Completamento step finali richiesti per email giorno 31-08-2023 tra Francesco Vissicchio e Nicola Cortese.

--- CODE/multistart_analogous.py
+++ CODE/multistart_analogous.py
@@ -26,36 +26,93 @@ def multistart(cost_function, N, params_lb_log, params_ub_log, t_data, data):
     x_star = None
     f_star = np.inf
 
+    success_count = 0
+
     for i in range(N):
-        print(f"Starting iteration {i + 1}")  # Check that the loop is running
+        print(f"\nMultiStart iteration {i + 1} of {N}")  # Check that the loop is running
 
         x_0 = random_point(params_lb_log, params_ub_log)
-        print("start point ", i+1, ": ", np.power(x_0,10))
+        print(f"Initial point: {np.power(10, x_0)}")
+        print(f"Lower bounds: {np.power(10, params_lb_log)}")
+        print(f"Upper bounds: {np.power(10, params_ub_log)}")
+
         func = lambda x_0: cost_function(t_data, x_0, data)
         out = fmincon_py(func, params_init_log=x_0, params_lb_log=params_lb_log, params_ub_log=params_ub_log,disp=False)
 
-        print(f"Optimizer finished, success: {out['success']}")  # Check that the optimizer is running
-        if out["success"] and out["fun"] < f_star:
-            x_star = out["opt_params"]
-            f_star = out["fun"]
+        print(f"Success: {out['success']}")  # Check that the optimizer is running
+        if out["success"]:
+            success_count += 1
+            print(f"Objective calculated value: {out['fun']}")
+            print(f"Optimal parameters: {out['opt_params']}")
+            if out["fun"] < f_star:
+                x_star = out["opt_params"]
+                f_star = out["fun"]
 
-        print(f'Iteration: {i + 1}, Best cost so far: {f_star}, Best params so far: {x_star}')
+        print(f'Iteration: {i + 1}, Best cost so far: {f_star}, Best params so far: {np.power(10, x_star)}')
 
-    print("Optimal parameters:", x_star)
+    print("\nCompleted MultiStart runs:", success_count, "out of", N)
     print("Minimal cost:", f_star)
+    print("Optimal parameters:", np.power(10, x_star))
 
     return x_star, f_star
-
 
 # Perform the multistart optimization
 params_optimal, minimal_cost = multistart(cost_function, 10, params_lb_log, params_ub_log, time, data)
 
 '''
-test di random_point
-1. verifica dei bound 
-2. matlab come funziona il random_point in multistart?
-3. np.random.uniform trova punti sempre diversi?
+Ultimi passaggi da fare:
 
-multistart_py deve dirmi quante esecuzioni hanno avuto successo e dirmi a che punto è passo passo
-"sono all'esecuzione 1 di 7 e i miei punti iniziali sono... il valore calcolato è... i parametri calcolati sono..."
+    1. Verificare che nell'esecuzione del MultiStart, ogni punto di partenza ottenuto sia sempre diverso;
+    2. Verificare che gli stessi siano sempre all'interno dei boundaries definiti;
+    3. Controllare con precisione come il MultiStart di MATLAB sceglie i punti random;
+    4. Aggiungere maggiori informazioni in uscita (print per l'utente) nel corso dell'esecuzione 
+
+        (es. per ogni iterazione
+            Multistart iteration 3 di N
+            Initial point: [ ... ... ]
+            Lower bounds: [ ... ... ]
+            Upper bounds: [ ... ... ]
+            Success: False/True (Se il calcolo ritorna "false", obj_value ed il vettore dei parametri non dovrebbe aggiornarsi per l'iterazione in questione)
+            Objective calculated value: ...
+            Optimal parameters: [ ... ... ]
+        )
+
+        (es. al termine delle N iterazioni
+            Completed MultiStart runs: (Numero delle iterazioni completate con successo) out to N
+            Objective calculated value: (Il valore migliore ottenuto)
+            Optimal parameters: (Migliori parametri associati al valore precedente)
+        )
+
+Riepilogo passaggi eseguiti:
+
+    1. La funzione np.random.uniform genera numeri casuali utilizzando un generatore di numeri pseudo-casuali, 
+       il che significa che sebbene i numeri sembrino casuali, vengono generati in modo deterministico. 
+       Tuttavia, con un buon generatore di numeri pseudo-casuali (come quello utilizzato da NumPy), 
+       le possibilità di generare esattamente lo stesso numero due volte sono estremamente ridotte, 
+       soprattutto per i numeri in virgola mobile.
+       Tuttavia, è importante capire che, in senso puramente matematico, 
+       la probabilità di generare ogni volta numeri completamente diversi non è mai del 100%.
+       Ciò è dovuto alla natura della casualità e alle infinite possibilità nella gamma dei numeri in virgola mobile.
+       Se si sta generando un gran numero di numeri casuali, è teoricamente possibile, 
+       anche se estremamente improbabile, che si possa generare lo stesso numero due volte. 
+       Ma per scopi pratici, in genere si può presupporre che ogni chiamata a 
+       np.random.uniform(params_lb_log, params_ub_log) genererà un numero diverso, 
+       purché params_lb_log e params_ub_log definiscano un intervallo che includa più di un numero possibile.
+       
+    2. La funzione np.random.uniform genera numeri casuali nell'intervallo specificato dai suoi due argomenti, 
+       che per impostazione predefinita sono low e high. Nella funzione random_point(params_lb_log, params_ub_log), 
+       params_lb_log rappresenta il limite inferiore e params_ub_log rappresenta il limite superiore.
+       I numeri generati saranno sempre maggiori o uguali a params_lb_log (il limite inferiore) 
+       e minori di params_ub_log (il limite superiore). Si tenga presente che il limite superiore è esclusivo, 
+       il che significa che i numeri generati non saranno uguali a params_ub_log, ma sempre strettamente inferiori.
+    
+    3. MultiStart funziona scegliendo punti di partenza casuali entro i bound constraints del problema. 
+       Dopodiché risolve il problema da ciascuno di questi punti iniziali utilizzando un risolutore locale.
+       Il metodo specifico utilizzato da MultiStart per scegliere questi punti di partenza non è 
+       esplicitamente documentato da MathWorks. 
+       Tuttavia, è ragionevole supporre che esso utilizzi una qualche forma di uniform random distribution 
+       entro i limiti di ciascuna variabile. Ciò darebbe la migliore possibilità di esplorare a fondo 
+       lo spazio del problema e di trovare l’ottimo globale.
+       
+    4. Ho effettuato delle modifiche al codice e adesso l'esecuzione procede come richiesto e con i print richiesti.
 '''
